ef591f12ce8d635500971a5b91122361cbb0967d
Kian Mayne <github@kianmayne.co.uk>
2018-09-08 10:46:01 +0100

    Hide full form

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -2,13 +2,13 @@
   <article>
   	<h1>Welcome</h1>
       <p>Welcome to the official Circle Line Pub Crawl app. This app will serve as your guide for the crawl and is where you'll be recording penalty points.</p>
-      <form>
+      <form v-if="currentUser.checkIns.length === 0" >
         <label>Please select the pub you're starting from: 
           <select v-model="selectedPub">
             <option v-for="pub in route" :value="pub.name">{{pub.name}}</option>
           </select>
         </label>
-        <button v-if="currentUser.checkIns.length === 0" id="#checkin-button" @click.prevent="checkIn(selectedPub)" class="submit-button">
+        <button id="#checkin-button" @click.prevent="checkIn(selectedPub)" class="submit-button">
           <span>Lets go!</span>
         </button>
       </form>
