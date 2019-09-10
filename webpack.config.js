f8451759b8a2d69190e041d1217be6eaa7a7abf8
Kian Mayne <git@kianmayne.co.uk>
2019-09-10 21:00:22 +0100

    Upgraded to webpack v4

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   entry: './src/webapp.js',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, './build'),
     publicPath: '/build/',
@@ -83,26 +84,8 @@ module.exports = {
   performance: {
     hints: false
   },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
   devtool: '#eval-source-map'
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
-}
+};
