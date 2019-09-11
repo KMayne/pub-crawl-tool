a5dc68a70e8ab20ac3ed37409ff79a9ec2964fd6
Kian Mayne <github@kianmayne.co.uk>
2019-09-11 23:30:29 +0100

    Fix typo in config comment

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   mongoURL: 'mongodb://localhost/pubcrawl',
   // API key for Google Maps (see https://developers.google.com/maps/documentation/javascript/get-api-key for details)
   googleMapsAPIKey: '[Google Maps API Key Here]',
-  // The URL the app will be accessible for - this is only for convenience for copying the
+  // The URL the app will be accessible for - this is only for convenience for copying the admin key
   serverURL: '[SERVER-URL]'
 };
